637a41a56ca9b9506a4ada2a929b88c34fa8e052
Mukul <mukulsomukesh@gmail.com>
2022-04-02 18:15:53 +0530

    navbar and footer add

---
+++ js/header.js
@@ -0,0 +1,32 @@
+
+    function RIngFun() {
+        var t = "Rings";
+        localStorage.setItem("Display_Category", t);
+        window.open("template.html", "_self");
+    }
+
+    function penFun() {
+        var t = "Necklaces_&_Pendants";
+        localStorage.setItem("Display_Category", t);
+        window.open("template.html", "_self");
+    }
+
+    function earFun() {
+        var t = "Earrings";
+        localStorage.setItem("Display_Category", t);
+        window.open("template.html", "_self");
+    }
+
+    function setsFun() {
+        var t = "Jewellery Sets";
+        localStorage.setItem("Display_Category", t);
+        window.open("template.html", "_self");
+    }
+
+function addCartFun(){
+    window.open("cart.html", "_self");
+}
+
+function wishlistFun(){
+    alert("wish list page");
+}
